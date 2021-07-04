eaf9db84875ec2fc5142a333be590213d3b5b44f
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-07-04 22:54:16 +1200

    convert to listView.builder from SingleChildScrollrable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,44 +10,44 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-            children: _transactions.map((tx) {
-          return Card(
-            child: Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.purple)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    )),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        height: 400,
+        child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                child: Row(
                   children: [
-                    Text(
-                      tx.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    Text(
-                      DateFormat('d/MM/y').format(tx.date),
-                      style: TextStyle(color: Colors.grey),
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${_transactions[index].amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          _transactions[index].title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          DateFormat('d/MM/y')
+                              .format(_transactions[index].date),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
-          );
-        }).toList()),
-      ),
-    );
+                ),
+              );
+            },
+            itemCount: _transactions.length));
   }
 }
