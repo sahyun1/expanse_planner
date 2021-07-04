c3dbcebf8dfed9493e312770715a41e7001aa9e0
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-07-04 22:44:00 +1200

    added scrolling to list with set height of parent

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,39 +9,45 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: _transactions.map((tx) {
-      return Card(
-        child: Row(
-          children: [
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.purple)),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+            children: _transactions.map((tx) {
+          return Card(
+            child: Row(
               children: [
-                Text(
-                  tx.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  DateFormat('d/MM/y').format(tx.date),
-                  style: TextStyle(color: Colors.grey),
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.purple)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                      DateFormat('d/MM/y').format(tx.date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        ),
-      );
-    }).toList());
+            ),
+          );
+        }).toList()),
+      ),
+    );
   }
 }
