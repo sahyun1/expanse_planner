729099c7622d10903ca7a8a2e523fdaebcc29348
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-07-05 22:38:59 +1200

    show image when no transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,43 +11,60 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-            itemBuilder: (context, index) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColor)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${_transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        )),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        child: _transactions.isEmpty
+            ? Column(
+                children: [
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  Container(
+                    height: 200,
+                    margin: EdgeInsets.only(top: 10),
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Row(
                       children: [
-                        Text(
-                          _transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          DateFormat('d/MM/y')
-                              .format(_transactions[index].date),
-                          style: TextStyle(color: Colors.grey),
+                        Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Theme.of(context).primaryColor)),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              '\$${_transactions[index].amount.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Theme.of(context).primaryColor),
+                            )),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _transactions[index].title,
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            Text(
+                              DateFormat('d/MM/y')
+                                  .format(_transactions[index].date),
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              );
-            },
-            itemCount: _transactions.length));
+                    ),
+                  );
+                },
+                itemCount: _transactions.length));
   }
 }
